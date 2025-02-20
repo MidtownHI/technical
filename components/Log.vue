0755208a0686c10eb31bd35b877973183c6002a6
Aaron J. Spurlock <2616003+div0ky@users.noreply.github.com>
2025-02-20 09:57:37 -0600

    250220-095737

---
+++ components/Log.vue
@@ -0,0 +1,14 @@
+<script setup>
+const count = ref(0);
+
+function handleClick() {
+  console.log("Before");
+  setTimeout(() => console.log("Timeout"), 0);
+  Promise.resolve().then(() => console.log("Promise"));
+  console.log("After");
+}
+</script>
+
+<template>
+  <button @click="handleClick">Click me</button>
+</template>
---
+++ components/Posts.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface Post {
+  id: number;
+  title: string;
+}
+
+const data = ref<Post[]>([])
+
+async function fetchData() {
+  data = await fetch("https://jsonplaceholder.typicode.com/posts").then(res => res.json())
+}
+</script>
+
+<template>
+  <div>
+    <button @click="fetchData">Load Posts</button>
+    <ul>
+      <li v-for="post in data" :key="post.id">{{ post.title }}</li> 
+    </ul>
+  </div>
+</template>
